824d025b6db0d3a582a3d6afa677114143018a37
aysvarya-gopinath <aysvarya.gopinath@colorado.edu>
2025-03-09 16:39:31 -0600

    updated

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -13,17 +13,6 @@ ifeq ($(BR2_PACKAGE_LDD),y)
 LDD_MODULE_SUBDIRS = misc-modules
 LDD_MODULE_SUBDIRS += scull
 
-define LDD_INSTALL_TARGET_CMDS
-#installing scull and misc modules from the ldd directory to run on the target 
-        $(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
-        $(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
-	
-	
-endef
-
-
 endif
 $(eval $(kernel-module))
 $(eval $(generic-package))
