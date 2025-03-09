4c5bcf13ce7e608c0f9825c22c52525927cd0ff6
aysvarya-gopinath <aysvarya.gopinath@colorado.edu>
2025-03-08 19:57:38 -0700

    full-test passes

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -10,18 +10,21 @@ LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
 ifeq ($(BR2_PACKAGE_LDD),y)
-	LDD_MODULE_SUBDIRS = misc-modules
-	LDD_MODULE_SUBDIRS += scull
+LDD_MODULE_SUBDIRS = misc-modules
+LDD_MODULE_SUBDIRS += scull
 
-	define LDD_INSTALL_TARGET_CMDS
-	#installing scull and misc modules from the ldd directory to run on the target 
+define LDD_INSTALL_TARGET_CMDS
+#installing scull and misc modules from the ldd directory to run on the target 
         $(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
         $(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
-	endef
-endif
+	
+	
+endef
 
+
+endif
 $(eval $(kernel-module))
 $(eval $(generic-package))
 
